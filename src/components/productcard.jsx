5b54852b8ea15e26a1c87ce6cc398b27d19a0be4
penumajji sravani <penumajjisravani698@gmail.com>
2025-03-18 10:22:38 +0530

    added product

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,23 @@
 // write product card here
+import React from 'react';
+import ViewProductButton from '../components/button';
+
+const ProductCard = () => {
+  const productImage = 'https://stylestryproductionwls47sou4z.cdn.e2enetworks.net/images/products/medium/512b6d17fc0254a947f9c0b1102d43cd957f93b3.webp';
+  const productName = 'Sports shoes for Women';
+  const productPrice = 'Rs. 1,000';
+
+  return (
+    <div className="product_card">
+      <img src={productImage} alt={productName} className="product_image" />
+
+      <h3 className="product_name">{productName}</h3>
+
+      <p className="product_price">{productPrice}</p>
+
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default ProductCard;
